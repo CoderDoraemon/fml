bfeecbc957f50483a181d5519eabf615ff8b9d6c
LDD <xfsrn@139.com>
2019-08-15 22:26:50 +0800

    no message

--- 仿糗事百科App/pages/index/index.vue
+++ 仿糗事百科App/pages/index/index.vue
@@ -1,6 +1,25 @@
 <template>
 	<view class="content">
-		
+		<view class="header">
+			<view class="avatar-name">
+				<image src="../../static/demo/userpic/1.jpg" mode="widthFix"></image>
+				<text>撸大湿</text>
+			</view>
+			<view class="attention-del">
+				<text>+ 关注</text>
+				<text>x</text>
+			</view>
+		</view>
+		<view class="title">
+			走出去，才发现你跟别人不一样
+		</view>
+		<view class="cover">
+			<image src="../../static/demo/datapic/1.jpg" mode="widthFix"></image>
+		</view>
+		<view class="icons">
+			<view class="like"></view>
+			<view class="comment"></view>
+		</view>
 	</view>
 </template>
 
